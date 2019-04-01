55f238013c176888a00b8ed93d756949b9799e04
Diego Gallegos <diegogallegos@Diegos-MacBook-Pro.local>
2019-03-31 21:29:05 -0600

    create new using gorm

--- go.mod
+++ go.mod
@@ -6,7 +6,10 @@ require (
 	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/go-playground/locales v0.12.1 // indirect
 	github.com/go-playground/universal-translator v0.16.0 // indirect
+	github.com/jinzhu/gorm v1.9.2
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
 	github.com/leodido/go-urn v1.1.0 // indirect
+	github.com/lib/pq v1.0.0 // indirect
 	github.com/treastech/logger v0.0.0-20180705232552-e381e9ecf2e3
 	go.uber.org/atomic v1.3.2 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
--- main.go
+++ main.go
@@ -7,18 +7,29 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	l "github.com/treastech/logger"
 	"go.uber.org/zap"
 	"gopkg.in/go-playground/validator.v9"
 )
 
 type Gym struct {
+	gorm.Model
 	Name string `validate:"required"`
 }
 
 var validate *validator.Validate
 
 func main() {
+	db, err := gorm.Open("postgres", "host=db port=5432 user=fitup dbname=fitup password=fitup sslmode=disable")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Gym{})
+
 	router := chi.NewRouter()
 	config := zap.NewProductionConfig()
 	config.OutputPaths = []string{"stdout"}
@@ -30,7 +41,7 @@ func main() {
 	router.Use(middleware.RequestID)
 	router.Use(l.Logger(logger))
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+		w.Write([]byte("welcome!!"))
 	})
 	router.Post("/", func(w http.ResponseWriter, r *http.Request) {
 		var gym Gym
@@ -38,6 +49,8 @@ func main() {
 		if err := validate.Struct(gym); err != nil {
 			logger.Warn("Error", zap.Error(err))
 		}
+		db.Create(&gym)
+		json.NewEncoder(w).Encode(&gym)
 	})
 	logger.Info("Running server in port 5000")
 	if err := http.ListenAndServe(":5000", router); err != nil {
